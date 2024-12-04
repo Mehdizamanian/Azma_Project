8ed417ea411267c5c7de0ae373460cacabc2b1ba
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-12-04 14:00:54 +0330

    Static files , templates , media folder rooted

--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,22 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include 
 
+# image configuration in debuging True
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+# main url core 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('echo.urls')),
 ]
+
+
+
+
+# url routing image configuration in debuging True
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
