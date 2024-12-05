296e62df73752e4b6288cabc324de7a23e81f5b0
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-12-05 13:04:57 +0330

    magical method added

--- echo/admin.py
+++ echo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(OsTyper)
+admin.site.register(MathCommand)
+admin.site.register(Parameter)
+admin.site.register(OsCommand)
+
+
--- echo/models.py
+++ echo/models.py
@@ -16,12 +16,22 @@ class OsCommand(models.Model):
   command_name=models.CharField(max_length=50)
   paramet = models.ForeignKey('Parameter', on_delete=models.PROTECT ,null=True)
 
+  def __str__(self):
+    return self.command_name
+
+
+
 class Parameter(models.Model):
   name=models.CharField(max_length=50)
+  def __str__(self):
+    return f"{self.name}"
 
 
 
 class MathCommand(models.Model):
   command_type=models.OneToOneField(OsTyper, on_delete=models.CASCADE , null=False,blank=False)
   expression=models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.expression
 
