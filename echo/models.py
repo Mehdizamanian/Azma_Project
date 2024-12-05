c0cefe04917a45ba364e2e8764355d0f595246f7
Mehdizamanian <zamanianmehdi23@gmail.com>
2024-12-05 12:56:50 +0330

    models created

--- echo/models.py
+++ echo/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+os_choices=(
+  ('oscammand','oscammand'),
+  ('mathcocommand','mathcocommand')
+)
+
+
+class OsTyper (models.Model):
+  name=models.CharField(choices=os_choices,max_length=50)
+
+
+
+class OsCommand(models.Model):
+  command_type=models.OneToOneField(OsTyper, on_delete=models.CASCADE,null=False,blank=False)
+  command_name=models.CharField(max_length=50)
+  paramet = models.ForeignKey('Parameter', on_delete=models.PROTECT ,null=True)
+
+class Parameter(models.Model):
+  name=models.CharField(max_length=50)
+
+
+
+class MathCommand(models.Model):
+  command_type=models.OneToOneField(OsTyper, on_delete=models.CASCADE , null=False,blank=False)
+  expression=models.CharField(max_length=50)
+
